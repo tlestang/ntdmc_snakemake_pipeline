c68471260a7f2d66e67ad9aaa7714d3ff55a95cc
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-05-19 11:02:40 +0100

    Actually call trachoma model, casting input paths to strings

--- scripts/resimulate.py
+++ scripts/resimulate.py
@@ -14,16 +14,14 @@ for iucode in sampled_params.columns:
     beta_input = pin / f"beta_values_{iucode}.csv"
     # Using dataframe index as seed value
     sampled_params[iucode].to_csv(beta_input, header=True, index=True)
-    with open(pout / f"prevalence_{iucode}.csv") as f:
-        f.write("prevalence output here")
-    with open(pout / f"infection_{iucode}.csv") as f:
-        f.write("infection output here")
-    # Trachoma_Simulation(
-    #     beta_input,
-    #     snakemake.input[1],
-    #     pout / f"prevalence_{iucode}.csv",
-    #     pout / f"infection_{iucode}.csv",
-    #     SaveOutput=True,
-    #     OutSimFilePath=pout / f"output_vals_{iucode}.p"
-    #     InSimFilePath=None
-    # )
+    # Have to convert paths to strings because simulation methods
+    # iterate on filename strings at some point
+    Trachoma_Simulation(
+        str(beta_input),
+        snakemake.input[1],
+        str(pout / f"prevalence_{iucode}.csv"),
+        str(pout / f"infection_{iucode}.csv"),
+        SaveOutput=True,
+        OutSimFilePath=str(pout / f"output_vals_{iucode}.p"),
+        InSimFilePath=None
+    )
